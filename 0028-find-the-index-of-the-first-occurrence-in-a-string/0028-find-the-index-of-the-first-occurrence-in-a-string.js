be17e86998da09d88e691ef0365473d40b685ae1
soumyasis king <92395483+Venturesum1@users.noreply.github.com>
2024-09-09 16:12:08 +0530

    Time: 60 ms (17.65%), Space: 48.8 MB (70.29%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -1,10 +1,10 @@
-class Solution {
-    public int strStr(String haystack, String needle) {
-        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
-            if(haystack.substring(i,j).equals(needle)){
-                return i;
-            }
-        }
-        return -1;
-    }
-}
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+
+    
+};
